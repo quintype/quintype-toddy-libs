bc218ad37f4452e1ce21be0f21b3922c981af050
Tejas Dinkar <tejas@gja.in>
2018-01-08 11:15:14 +0530

    Fixing a bug where API clients weren't reused

--- server/api-client.js
+++ server/api-client.js
@@ -9,7 +9,7 @@ function getClient(hostname) {
 }
 
 function createTemporaryClient(hostname) {
-  const matchedString = (config.host_to_automatic_api_host || []).some(str => hostname.includes(str) && str);
+  const matchedString = (config.host_to_automatic_api_host || []).find(str => hostname.includes(str));
   if(matchedString)
     return new Client(`https://${hostname.replace(matchedString, "")}`, true);
 }
