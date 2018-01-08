d6045144d78529b71cd0b4ff39a082c2dc03d5f6
Tejas Dinkar <tejas@gja.in>
2018-01-08 22:31:19 +0530

    Start also logs correctly

--- server/start.js
+++ server/start.js
@@ -1,6 +1,7 @@
 var cluster = require('cluster');
 var process = require("process");
 const {initializeAllClients} = require("./api-client");
+const logger = require("./logger");
 
 module.exports.startApp = function(appThunk, opts = {}) {
   if(cluster.isMaster) {
@@ -9,7 +10,7 @@ module.exports.startApp = function(appThunk, opts = {}) {
       cluster.fork();
     }
     cluster.on('exit', function(worker, code, signal) {
-      console.log('worker ' + worker.process.pid + ' died');
+      logger.error('worker ' + worker.process.pid + ' died');
       cluster.fork();
     });
   } else {
@@ -22,8 +23,7 @@ module.exports.startApp = function(appThunk, opts = {}) {
       .then(() => app.listen(opts.port || 3000, () => console.log('Example app listening on port 3000!')))
       .catch(function(e) {
         var sleep = require("sleep-promise");
-        console.error("Worker died - Aborting");
-        console.error(e.stack);
+        logger.error("Worker died - Aborting", {stack: e.stack});
         return new Promise((resolve) => resolve(cluster.worker.disconnect()))
           .then(() => sleep(opts.sleep || 250))
           .then(() => process.exit());
