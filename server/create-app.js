c40b9e88f8ae5d87bc9f2fbfd828be07c84f6257
Tejas Dinkar <tejas@gja.in>
2018-01-30 23:57:19 +0530

    Testing the create-app function

--- server/create-app.js
+++ server/create-app.js
@@ -4,11 +4,14 @@ const morgan = require("morgan");
 
 const logger = require("./logger");
 
-function createApp() {
+function createApp({disableLogging = false} = {}) {
   const app = express.apply(this, arguments);
   app.set("view engine", "ejs");
 
-  app.use(morgan("short", {stream: {write: msg => logger.info(msg)}}))
+  // istanbul ignore if
+  if(!disableLogging) {
+    app.use(morgan("short", {stream: {write: msg => logger.info(msg)}}))
+  }
 
   app.use(express.static("public"));
   app.use(compression());
---
+++ test/integration/create-app-test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+const createApp = require("../../server/create-app");
+const supertest = require("supertest");
+const fs = require("fs");
+
+describe('createApp', function() {
+  const app = createApp({disableLogging: true});
+  app.get("/foo", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(fs.readFileSync("package.json"));
+  })
+
+  it("correctly gzips content", function(done) {
+    supertest(app)
+      .get("/foo")
+      .set("Accept-Encoding", "deflate, gzip")
+      .expect("Transfer-Encoding", /chunked/)
+      .expect("Content-Encoding", /gzip/)
+      .expect(200, done)
+  })
+});
