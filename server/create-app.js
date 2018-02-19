ff7bcef864c83d0a32259a94efec74b9f5e8c69a
Akshar Dave <akshar314@gmail.com>
2018-02-19 15:59:55 +0530

    WIP- express-session

--- server/create-app.js
+++ server/create-app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const compression = require("compression");
 const morgan = require("morgan");
-
+const session = require("express-session");
 const logger = require("./logger");
 
 function createApp({assetHelper = require("./asset-helper"), publicFolder = "public"} = {}) {
@@ -9,6 +9,18 @@ function createApp({assetHelper = require("./asset-helper"), publicFolder = "pub
   app.set("view engine", "ejs");
 
   app.use(morgan("short", {stream: {write: msg => logger.info(msg)}}))
+
+  app.use(session({
+    name: 'server-session-cookie-id',
+    secret: 'my express secret',
+    saveUninitialized: true,
+    resave: true
+  }));
+
+  app.use((req, res, next) => {
+    console.log(req.session, "user session")
+    return next();
+  });
 
   const assetFiles = assetHelper.assetFiles();
 
