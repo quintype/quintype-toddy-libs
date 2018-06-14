359ff03e1e9bceacde07f11235b3fbac22bbf336
Ankit Tewari <ankt271192@users.noreply.github.com>
2018-06-14 16:33:21 +0530

    Add entities

--- server/impl/api-client-impl.js
+++ server/impl/api-client-impl.js
@@ -1,4 +1,4 @@
-const { Client, Story, Author, Member, Collection } = require("quintype-backend");
+const { Client, Story, Author, Member, Collection, Entity } = require("quintype-backend");
 const { storyToCacheKey, collectionToCacheKey, authorToCacheKey, sorterToCacheKey } = require("../caching");
 const _ = require("lodash");
 
@@ -32,4 +32,4 @@ Story.prototype.cacheKeys = function(publisherId) {
 
 Story.sorterToCacheKey = sorterToCacheKey;
 
-module.exports = {getClientImpl, Client, Story, Author, Member, Collection};
+module.exports = {getClientImpl, Client, Story, Author, Member, Collection, Entity};
