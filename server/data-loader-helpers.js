977500a176dda01b5047871ccdc17b6879e12e89
Tejas Dinkar <tejas@gja.in>
2018-07-07 00:39:37 +0530

    Adding helpers to load homepage collection too

--- server/data-loader-helpers.js
+++ server/data-loader-helpers.js
@@ -1,9 +1,9 @@
 // istanbul ignore file
 
-const {Story} = require('./api-client');
+const {Story, Collection} = require('./api-client');
 const templateOptions = require('./template-options');
 
-exports.catalogDataLoader = function(client, config) {
+exports.catalogDataLoader = function catalogDataLoader(client, config) {
   return Story.getStories(client)
     .then(stories => {
       return {
@@ -12,4 +12,24 @@ exports.catalogDataLoader = function(client, config) {
         templateOptions
       }
     });
+}
+
+exports.homeCollectionOrStories = function homeCollectionOrStories(client) {
+  return Collection.getCollectionBySlug(client, "home", { 'item-type': 'collection'}, {depth: 1})
+    .then(collection => {
+      if(collection)
+        return collection;
+      else
+        return Story.getStories(client).then(stories => Collection.build({
+          slug: 'home',
+          name: "Home",
+          template: "default",
+          items: [{
+            type: 'collection',
+            name: "Home",
+            template: "default",
+            items: stories.map(story => ({type: 'story', story: story.asJson()}))
+          }]
+        }))
+    })
 }
