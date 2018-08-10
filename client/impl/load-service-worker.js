990194894060c88a0794721fcf087977d44358ff
Jeevan Kishore <kishor080@gmail.com>
2018-08-10 16:17:30 +0530

    add global to caches access

--- client/impl/load-service-worker.js
+++ client/impl/load-service-worker.js
@@ -40,7 +40,7 @@ export function checkForServiceWorkerUpdates(app, page = {}) {
     const {config:{'theme-attributes': pageThemeAttributes = {}} = {}} = page;
     if((pageThemeAttributes['cache-burst'] || 0) > parseInt(configVersion)) {
       console.log(`updating service worker due to config change`);
-      caches && caches.keys().then(cacheField => {
+      global.caches && global.caches.keys().then(cacheField => {
         console.log('Deleting cache files');
         cacheField.forEach(c=>caches.delete(c));
       });
