cdc34c710c632a10eadd33c08a634e70fc90e61f
Garima Kamboj <garima.kamboj@icloud.com>
2018-08-14 10:08:37 +0530

    Put custom-urls to work

--- server/handlers/custom-route-handler.js
+++ server/handlers/custom-route-handler.js
@@ -4,31 +4,15 @@ const {createStore} = require("redux");
 const { Static } = require("../api-client");
 
 exports.handleCustomRoute = function handleCustomRoute(req, res, next, { config, client, renderLayout }) {
-    const url = urlLib.parse(req.url, true);
+  const url = urlLib.parse(req.url, true);
 
-    Static.getStaticData(client, req.params[0])
+  Static.getStaticData(client, req.params[0])
     .then(response => {
-        if(response.page.type === 'redirect') { 
-          res.redirect(301, response.page["destination-path"]);
-        }
-        if(response.page.type === 'static-page') {
-          if(response.page.metadata.header && response.page.metadata.footer) {
-            const store = createStore((state) => state, {
-              qt: {
-                pageType: response.page.type,
-                data: response.page,
-                config: config,
-                currentPath: `${url.pathname}${url.search || ""}`
-              }
-            });
-
-            return renderLayout(res, {
-                contentTemplate: './custom-static-page',
-                store: store
-            })    
-          }
-          res.send(response.page);
-        }
+      if(!response) {
+        next();
+      }
+      if(response.page.type === 'redirect') { 
+        return res.redirect(301, response.page["destination-path"]);
+      }
     })
-    .catch(err => next())
 }
