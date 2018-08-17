de7494a45271bfd7833907ea6473afe7c5e28757
Garima Kamboj <garima.kamboj@icloud.com>
2018-08-17 15:56:24 +0530

    Add custom-route-handler tests for 301, 302, 404 api response

--- test/integration/custom-route-handler-test.js
+++ test/integration/custom-route-handler-test.js
@@ -9,7 +9,16 @@ function getClientStub(hostname) {
   return {
     getHostname: () => "demo.quintype.io",
     getConfig: () => Promise.resolve({foo: "bar"}),
-    getCustomPathData: () => Promise.resolve({"page":{"type":"redirect","status-code":301,"destination-path":"/foobar"}}),
+    getCustomPathData: (path) => {
+      switch(path) {
+        case '/moved-p':
+          return Promise.resolve({"page":{"type":"redirect","status-code":301,"destination-path":"/moved-p-path"}});
+        case '/moved-t':
+          return Promise.resolve({"page":{"type":"redirect","status-code":302,"destination-path":"/moved-t-path"}});
+        default:
+          return Promise.resolve({"page":null,"status-code":404});               
+      }
+    },
   }
 }
 
@@ -19,7 +28,8 @@ function createApp(loadData, routes, opts = {}) {
     assetHelper: {assetHash: (file) => file == "app.js" ? "abcdef" : null, assetPath: (file) => `/assets/${file}`},
     getClient: getClientStub,
     generateRoutes: () => routes,
-    loadData: loadData,
+    loadData: loadData,    
+    handleNotFound: false
   }, opts));
   
   return app;
@@ -27,11 +37,26 @@ function createApp(loadData, routes, opts = {}) {
 
 
 describe('Custom Route Handler', function() {
-  it("Redirects with proper status code if API has a redirection setup", function(done) {
+  it("Redirects with status code 301 if API has a redirection setup", function(done) {
     const app = createApp((pageType, params, config, client, {host, next}) => next(), [{pageType: 'story-page', path: '/*'}]);
     supertest(app)
-      .get("/no-longer-here")
-      .expect("Location", "/foobar")
+      .get("/moved-p")
+      .expect("Location", "/moved-p-path")
       .expect(301, done);
+  });
+
+  it("Redirects with status code 302 if API has a redirection setup", function(done) {
+    const app = createApp((pageType, params, config, client, {host, next}) => next(), [{pageType: 'story-page', path: '/*'}]);
+    supertest(app)
+      .get("/moved-t")
+      .expect("Location", "/moved-t-path")
+      .expect(302, done);
+  });
+
+  it("Returns 404 if the route doesn't exist", function(done) {
+    const app = createApp((pageType, params, config, client, {host, next}) => next(), [{pageType: 'story-page', path: '/*'}]);
+    supertest(app)
+      .get("/does-not-exist")
+      .expect(404, done);
   });
 })
