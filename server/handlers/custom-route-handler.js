b0674473f26ce826befb2f32758ba612dec7c135
Garima Kamboj <garima.kamboj@icloud.com>
2018-08-22 15:37:46 +0530

    Use addCacheHeadersToResult to add cache-keys

--- server/handlers/custom-route-handler.js
+++ server/handlers/custom-route-handler.js
@@ -2,6 +2,7 @@ const urlLib = require("url");
 const {createStore} = require("redux");
 
 const { CustomPath } = require("../impl/api-client-impl");
+const { addCacheHeadersToResult } = require("./cdn-caching");
 
 exports.customRouteHandler = function customRouteHandler(req, res, next, { config, client, renderLayout, logError, seo}) {
   const url = urlLib.parse(req.url, true);
@@ -32,14 +33,8 @@ exports.customRouteHandler = function customRouteHandler(req, res, next, { confi
           const seoInstance = (typeof seo == 'function') ? seo(config) : seo;
           const seoTags = seoInstance && seoInstance.getMetaTags(config, page.type, {}, {url});
           
-          const cacheKey = "static";
-          res.setHeader('Cache-Control', "public,max-age=15,s-maxage=240,stale-while-revalidate=300,stale-if-error=14400");
-          res.setHeader('Vary', "Accept-Encoding");
-          // Cloudflare Headers
-          res.setHeader('Cache-Tag', cacheKey);
-          // Fastly Header
-          res.setHeader('Surrogate-Control', "public,max-age=240,stale-while-revalidate=300,stale-if-error=14400");
-          res.setHeader('Surrogate-Key', cacheKey);
+          const cacheKeys = ["static"];
+          addCacheHeadersToResult(res, cacheKeys);
 
           res.status(page["status-code"]);
 
