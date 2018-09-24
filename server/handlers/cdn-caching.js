e05845b948ef5d209d8476389a4285c55a450ad4
Abhishek Jaiswal <jabhi207@gmail.com>
2018-09-24 16:46:54 +0530

    cache-control for PRIVATE cache-keys

--- server/handlers/cdn-caching.js
+++ server/handlers/cdn-caching.js
@@ -2,19 +2,26 @@ const _ = require("lodash");
 
 exports.addCacheHeadersToResult = function addCacheHeadersToResult(res, cacheKeys) {
   if(cacheKeys) {
-    res.setHeader('Cache-Control', "public,max-age=15,s-maxage=240,stale-while-revalidate=300,stale-if-error=14400");
-    res.setHeader('Vary', "Accept-Encoding");
+    if(cacheKeys === 'PRIVATE'){
+      res.setHeader('Cache-Control', "private,no-cache,no-store");
+      res.setHeader('Vary', "Accept-Encoding");
+      res.setHeader('Surrogate-Control', "private,no-cache,no-store");
+    } else {
+      res.setHeader('Cache-Control', "public,max-age=15,s-maxage=240,stale-while-revalidate=300,stale-if-error=14400");
+      res.setHeader('Vary', "Accept-Encoding");
 
-    // Cloudflare Headers
-    res.setHeader('Cache-Tag', _(cacheKeys).uniq().join(","));
+      // Cloudflare Headers
+      res.setHeader('Cache-Tag', _(cacheKeys).uniq().join(","));
+      
+      // Fastly Header
+      res.setHeader('Surrogate-Control', "public,max-age=240,stale-while-revalidate=300,stale-if-error=14400");
+      res.setHeader('Surrogate-Key', _(cacheKeys).uniq().join(" "));
+    }
     
-    // Fastly Header
-    res.setHeader('Surrogate-Control', "public,max-age=240,stale-while-revalidate=300,stale-if-error=14400");
-    res.setHeader('Surrogate-Key', _(cacheKeys).uniq().join(" "));
   } else {
-    res.setHeader('Cache-Control', "private,no-cache");
+    res.setHeader('Cache-Control', "public,max-age=15,s-maxage=16");
     res.setHeader('Vary', "Accept-Encoding");
-    res.setHeader('Surrogate-Control', "private,no-cache");
+    res.setHeader('Surrogate-Control', "public,max-age=15,s-maxage=16");
   }
   return res;
 }
