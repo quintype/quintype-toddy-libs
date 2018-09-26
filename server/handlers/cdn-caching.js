51fe76116e3016f311e22d734f0d8f23a8ea05c2
Amith George <strider.am@gmail.com>
2018-09-26 22:52:52 +0530

    updated default s-maxage to 60s

--- server/handlers/cdn-caching.js
+++ server/handlers/cdn-caching.js
@@ -12,16 +12,16 @@ exports.addCacheHeadersToResult = function addCacheHeadersToResult(res, cacheKey
 
       // Cloudflare Headers
       res.setHeader('Cache-Tag', _(cacheKeys).uniq().join(","));
-      
+
       // Fastly Header
       res.setHeader('Surrogate-Control', "public,max-age=240,stale-while-revalidate=300,stale-if-error=14400");
       res.setHeader('Surrogate-Key', _(cacheKeys).uniq().join(" "));
     }
-    
+
   } else {
-    res.setHeader('Cache-Control', "public,max-age=15,s-maxage=15");
+    res.setHeader('Cache-Control', "public,max-age=15,s-maxage=60");
     res.setHeader('Vary', "Accept-Encoding");
-    res.setHeader('Surrogate-Control', "public,max-age=15,s-maxage=15");
+    res.setHeader('Surrogate-Control', "public,max-age=15,s-maxage=60");
   }
   return res;
 }
