21c4704b57cb44f0da2574abd0e8d9fd84c2fcf8
Shreya shukla <shreyaa1@users.noreply.github.com>
2018-12-07 13:27:43 +0530

    feat(Visual story): Add route for the same (#54)
    * add route, handler for visual story
    
    * refactor handleVisualStoryRoute
    
    * handle error
    
    * change route to map ampstories

--- server/handlers/isomorphic-handler.js
+++ server/handlers/isomorphic-handler.js
@@ -41,6 +41,9 @@ function loadDataForIsomorphicRoute(loadData, loadErrorData, url, routes, {other
   }
 }
 
+
+
+
 function loadDataForPageType(loadData, loadErrorData = () => Promise.resolve({httpStatusCode: 500}), pageType, params, {config, client, logError, host}) {
   return new Promise((resolve) => resolve(loadData(pageType, params, config, client, {host, next: abortHandler})))
     .then(result => {
@@ -93,6 +96,16 @@ function createStoreFromResult(url, result, opts = {}) {
   };
   return createBasicStore(result, qt, opts);
 }
+
+exports.handleVisualStoryRoute = function handleVisualStoryRoute(req, res, next, {config, client, renderVisualStory, loadData, pickComponent, loadErrorData, seo, logError, assetHelper}) {
+  if(!renderVisualStory) {
+    return res.send({error: "template unavailable"});
+  }
+  const pageType = 'visual-story';
+  const slug = _.get(req, ['params', [0]], '');
+  return loadDataForPageType(loadData, loadErrorData, pageType, {slug}, {config, client, logError, host: req.hostname})
+    .then(result => renderVisualStory(res, Object.assign({pageType: pageType, disableIsomorphicComponent: true}, result)));
+};
 
 exports.handleIsomorphicDataLoad = function handleIsomorphicDataLoad(req, res, next, {config, client, generateRoutes, loadData, loadErrorData, logError, staticRoutes, seo, appVersion}) {
   const url = urlLib.parse(req.query.path || "/", true);
@@ -165,7 +178,7 @@ exports.handleIsomorphicDataLoad = function handleIsomorphicDataLoad(req, res, n
         res.setHeader("Cache-Control", "public,max-age=15,s-maxage=120");
         res.setHeader("Vary", "Accept-Encoding");
         res.json(result)
-      }).catch(handleException).finally(() => res.end());;
+      }).catch(handleException).finally(() => res.end());
   }
 };
 
