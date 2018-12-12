533ed712ea2b1d0cf0d43929b92834076017221f
Tejas Dinkar <tejas@gja.in>
2018-12-12 07:47:57 +0530

    Adding more visual stories tests

--- test/integration/visual-story-test.js
+++ test/integration/visual-story-test.js
@@ -4,16 +4,23 @@ const express = require("express");
 const { enableVisualStories } = require("../../server/visual-stories");
 const supertest = require("supertest");
 
-function getClientStub(hostname, relatedStories = []) {
+function getClientStub(hostname) {
   return {
     getHostname: () => hostname,
-    getConfig: () => Promise.resolve({ "publisher-settings": { title: "Madrid" } }),
-    getRelatedStories: () => ({'related-stories': relatedStories}),
+    getConfig: () => Promise.resolve({ "publisher-settings": { title: "Madrid" }, 'publisher-id': 42 }),
   }
 }
 
-getClientStub.withRelatedStories = function(relatedStories) {
-  return hostname => getClientStub(hostname, relatedStories);
+getClientStub.withRelatedStories = relatedStories => {
+  return hostname => Object.assign({}, getClientStub(hostname), {
+    getRelatedStories: async () => ({'related-stories': relatedStories})
+  })
+}
+
+getClientStub.withStory = story => {
+  return hostname => Object.assign({}, getClientStub(hostname), {
+    getStoryBySlug: async () => ({story: story})
+  })
 }
 
 describe('Visual Stories Bookend', function () {
@@ -36,7 +43,7 @@ describe('Visual Stories Bookend', function () {
 
   it("returns a 404 if there are no related stories", function (done) {
     const app = express();
-    enableVisualStories(app, () => {}, {getClient: getClientStub})
+    enableVisualStories(app, () => {}, {getClient: getClientStub.withRelatedStories([])})
     supertest(app)
       .get("/ampstories/unknown/bookend.json")
       .expect("Content-Type", /json/)
@@ -44,3 +51,39 @@ describe('Visual Stories Bookend', function () {
       .then(() => done());
   });
 });
+
+describe("Visual Stories AmpPage", function() {
+  it("calls render if the story is present and adds caching headers", function(done) {
+    const story = {headline: "foobar", id: "abcdefgh", "story-template": "visual-story"};
+    const app = express();
+    enableVisualStories(app, (res, story) => { res.json(story.asJson()) }, { getClient: getClientStub.withStory(story) })
+    supertest(app)
+      .get("/ampstories/section/slug")
+      .expect("Cache-Control", /public/)
+      .expect("Cache-Tag", "s/42/abcdefgh")
+      .expect(200)
+      .then(res => {
+        const {headline} = JSON.parse(res.text);
+        assert.equal("foobar", headline)
+      })
+      .then(done);
+  })
+
+  it("returns 404 if the story is not found", function(done) {
+    const app = express();
+    enableVisualStories(app, () => { }, { getClient: getClientStub.withStory(null) })
+    supertest(app)
+      .get("/ampstories/section/slug")
+      .expect(404)
+      .then(() => done());
+  })
+
+  it("returns 404 if the story is not a visual story", function(done) {
+    const app = express();
+    enableVisualStories(app, () => { }, { getClient: getClientStub.withStory({'story-template': 'blank'}) })
+    supertest(app)
+      .get("/ampstories/section/slug")
+      .expect(404)
+      .then(() => done());
+  })
+})
