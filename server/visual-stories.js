eea7c6203311f0bbfafe4f94b51293ff0d2c6205
Jeevan Kishore <kishor080@gmail.com>
2018-12-12 15:51:34 +0530

    add seo support

--- server/visual-stories.js
+++ server/visual-stories.js
@@ -2,6 +2,7 @@ const { getWithConfig, withError} = require("./routes");
 const {Story} = require("./impl/api-client-impl");
 const {addCacheHeadersToResult} = require("./handlers/cdn-caching");
 const {storyToCacheKey} = require("./caching");
+const urlLib = require("url");
 
 async function handleBookend(req, res, next, {config, client}) {
   const relatedStoriesResponse = await client.getRelatedStories(req.params.storyId, req.query.section);
@@ -50,21 +51,26 @@ async function handleBookend(req, res, next, {config, client}) {
     }
   };
 
-  async function handleVisualStory(req, res, next, {config, client, renderVisualStory}) {
+  async function handleVisualStory(req, res, next, {config, client, renderVisualStory, seo}) {
+
+    const url = urlLib.parse(req.url, true);
     const story = await Story.getStoryBySlug(client, req.params.storySlug);
+
+    const seoInstance = (typeof seo === 'function') ? seo(config) : seo;
+    const seoTags = seoInstance && seoInstance.getMetaTags(config, story['story-template'], story, {url});
 
     if(story == null || story['story-template'] !== 'visual-story') {
       res.status(404);
       res.end();
     } else {
       addCacheHeadersToResult(res, [storyToCacheKey(config["publisher-id"], story)]);
-      await renderVisualStory(res, story, {config, client});
+      await renderVisualStory(res, story, {config, client, seoTags});
     }
   }
 
 
 
-  exports.enableVisualStories = function enableVisualStories(app, renderVisualStory, {logError, getClient}){
+  exports.enableVisualStories = function enableVisualStories(app, renderVisualStory, {logError, getClient, seo}){
     getWithConfig(app, "/ampstories/:storyId/bookend.json", withError(handleBookend, logError), {logError, getClient});
-    getWithConfig(app, "/ampstories/*/:storySlug", withError(handleVisualStory, logError), {logError, getClient, renderVisualStory});
+    getWithConfig(app, "/ampstories/*/:storySlug", withError(handleVisualStory, logError), {logError, getClient, renderVisualStory, seo});
   }
