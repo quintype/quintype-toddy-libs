c371e25b6c3a2690d5883752d8af6c0a7aade607
Garima Kamboj <garima.kamboj@icloud.com>
2019-08-23 15:22:27 +0530

    Update readAsset n add warmupAssetCache

--- server/impl/asset-helper-impl.js
+++ server/impl/asset-helper-impl.js
@@ -2,22 +2,21 @@ const flatMap = require('lodash/flatMap');
 const uniq = require('lodash/uniq');
 
 class AssetHelperImpl {
-  constructor(config, assets, {readFileSync, readFile} = {}) {
+  constructor(config, assets, {readFileSync} = {}) {
     this.config = config;
     this.assets = assets;
     this.readFileSync = readFileSync || require("fs").readFileSync;
-    this.readFile = readFile || require("fs").readFile;
     this.assetsContent = {};
 
     this.assetPath = this.assetPath.bind(this);
     this.readAsset = this.readAsset.bind(this);
-    this.readAssetAsync = this.readAssetAsync.bind(this);
     this.assetHash = this.assetHash.bind(this);
     this.assetFiles = this.assetFiles.bind(this);
     this.getChunk = this.getChunk.bind(this);
     this.getAllChunks = this.getAllChunks.bind(this);
     this.getFilesForChunks = this.getFilesForChunks.bind(this);
     this.serviceWorkerContents = this.serviceWorkerContents.bind(this);
+    this.warmupAssetCache = this.warmupAssetCache.bind(this);
   }
 
   assetPath(asset, host = this.config.asset_host) {
@@ -29,22 +28,20 @@ class AssetHelperImpl {
 
   readAsset(asset) {
     const path = this.assets[asset];
-    if (path) {
-      return this.readFileSync("public" + path);
+    
+    if(!path) {
+      return undefined;
     }
+    
+    if(!this.assetsContent[path]) {
+      this.assetsContent[path] = this.readFileSync("public" + path);
+    }
+    
+    return this.assetsContent[path];
   }
 
-  async readAssetAsync(asset) {
-    const path = this.assets[asset];
-    if(path) {
-      if(!this.assetsContent[path]) {
-        await this.readFile("public" + path, (err, data) => {
-          if (err) throw err;
-          this.assetsContent[path] = data;
-        })
-      }
-      return this.assetsContent[path];
-    }
+  warmupAssetCache() {
+    this.getAllChunks.apply(this, arguments);
   }
 
   getChunk(chunk) {
@@ -53,14 +50,12 @@ class AssetHelperImpl {
       const assets = this.assets[`${chunk}.${ext}`]
         ? [`${chunk}.${ext}`]
         : []
-
       return assets.concat(Object.keys(this.assets).filter(asset => regex.test(asset)));
     }
-
     return {
       cssFiles: getDependentFiles('css').map(file => ({
         path: this.assetPath(file),
-        content: this.readAssetAsync(file)
+        content: this.readAsset(file)
       })),
       jsPaths: getDependentFiles('js').map(file => this.assetPath(file))
     };
