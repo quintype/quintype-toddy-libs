f8a51148a394cad263a3b0b401569aa4985f88c5
Garima Kamboj <garima.kamboj@icloud.com>
2019-12-12 16:18:28 +0530

    Update type and remove extra code

--- client/impl/load-service-worker.js
+++ client/impl/load-service-worker.js
@@ -11,14 +11,13 @@ export function registerServiceWorker({enableServiceWorker = false, serviceWorke
 export function setupServiceWorkerUpdates(serviceWorkerPromise, app, store, page) {
   if(!serviceWorkerPromise)
     return Promise.resolve();
-  console.log("setup service worker ------------>");
+    
   return serviceWorkerPromise
     .then(registration => {
       if(!registration)
         return;
 
       if(registration.update) {
-        console.log("added updateServiceWorker --------------->");
         app.updateServiceWorker = () => registration.update().then(() => store.dispatch({type: SERVICE_WORKER_UPDATED}));
       }
       checkForServiceWorkerUpdates(app, page);
@@ -28,7 +27,6 @@ export function setupServiceWorkerUpdates(serviceWorkerPromise, app, store, page
 }
 
 export function checkForServiceWorkerUpdates(app, page = {}) {
-  console.log("coming in checkForServiceWorkerUpdates ------------>", app.getAppVersion(), page.appVersion);
   if((page.appVersion && app.getAppVersion && app.getAppVersion() < page.appVersion)) {
     console && console.log("Updating the Service Worker");
     app.updateServiceWorker && app.updateServiceWorker();
@@ -37,12 +35,9 @@ export function checkForServiceWorkerUpdates(app, page = {}) {
   /* Check if the config is updated and update the service worker if true */
   else if(global && global.qtVersion) {
     const {qtVersion: {configVersion = 0, pbConfigVersion = 0} = {}} = global;
-    console.log('global qt version------------>', global.qtVersion);
-    console.log('page config --------->', page.config);
     const {config:{'theme-attributes': pageThemeAttributes = {}, 'pagebuilder-config': pbConfig = {}} = {}} = page;
-    console.log("pageThemeAttributes['cache-burst'] ---------->", pageThemeAttributes['cache-burst'], testVersion);
-    console.log("parseInt(configVersion) --------------->", parseInt(configVersion), routeTestVersion);
-    if(((pageThemeAttributes['cache-burst'] || 0) > parseInt(configVersion)) || (pbConfig.configVersion > pbConfigVersion) ) {
+    
+    if(((pageThemeAttributes['cache-burst'] || 0) > parseInt(configVersion)) || (pbConfig.configVersion > parseInt(pbConfigVersion)) ) {
       console.log(`updating service worker due to config change`);
       app.updateServiceWorker && app.updateServiceWorker();
     }
