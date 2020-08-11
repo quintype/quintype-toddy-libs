41495ab5cdc4c1dbf08e5b1852024b72d4b59e3f
nandap4790 <nandap4790@gmail.com>
2020-08-11 17:35:50 +0530

    wip: test csp-1

--- server/create-app.js
+++ server/create-app.js
@@ -42,7 +42,7 @@ function createApp({
         if (assetFiles.has(res.req.url)) {
           res.set("Cache-Control", "public,max-age=31104000,s-maxage=31104000");
         }
-        res.set("Content-Security-Policy", "script-src *");
+        res.set("Content-Security-Policy", "script-src https://cdn.ampproject.org https://assets.prothomalo.com/ https://fea.assettype.com/");
         res.set("Vary", "Accept-Encoding");
       },
       maxAge: "1h",
