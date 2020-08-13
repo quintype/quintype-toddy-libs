89c25b0ee454d02f8e0c818e477d858b9e10e95f
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-08-13 11:31:10 +0530

    fix: add amp optimizer express middleware

--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,7 @@
  * @module routes
  */
 
+const AmpOptimizerMiddleware = require("@ampproject/toolbox-optimizer-express");
 const { generateServiceWorker } = require("./handlers/generate-service-worker");
 const {
   handleIsomorphicShell,
@@ -555,6 +556,14 @@ exports.ampRoutes = (app, opts = {}) => {
     handleAmpRequest,
     handleInfiniteScrollRequest,
   } = require("./handlers/amp-handler");
+
+  // This is a middleware. The same route should be matched after this by amp handler
+  app.get(
+    "/amp/story/*",
+    AmpOptimizerMiddleware.create({
+      verbose: true,
+    })
+  );
 
   getWithConfig(app, "/amp/story/*", handleAmpRequest, opts);
   getWithConfig(
