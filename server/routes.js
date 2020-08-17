a057cc5fe62e283ec6c86d52438434fb79e135a8
Deokumarjnu <deo.kumar.jnu@gmail.com>
2020-08-17 13:10:21 +0530

    added ex for redirect

--- server/routes.js
+++ server/routes.js
@@ -257,7 +257,7 @@ function getWithConfig(app, route, handler, opts = {}) {
  * @param {boolean|function} opts.lightPages If set to true, then all story pages will render amp pages.
  * @param {string} opts.cdnProvider The name of the cdn provider. Supported cdn providers are akamai, cloudflare. Default value is cloudflare.
  * @param {function} opts.maxConfigVersion An async function which resolves to a integer version of the config. This defaults to config.theme-attributes.cache-burst
- * @param {Array<object>|function} opts.redirectUrls An array or async function which used to render the redirect url provided in the array of object
+ * @param {Array<object>|function} opts.redirectUrls An array or async function which used to render the redirect url provided in the array of object - >ex- REDIRECT_URLS = [{sourceUrl: "/tag/:tagSlug",destinationUrl: "/topic/:tagSlug",statusCode: 301,}]
  */
 exports.isomorphicRoutes = function isomorphicRoutes(
   app,
