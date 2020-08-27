3656ce7ef8c623838072b58ee31b645713488b17
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-08-27 17:26:58 +0530

    fix(amp): remove ampOptimizer middleware, add amp toolbox optimizer

--- server/handlers/amp-handler.js
+++ server/handlers/amp-handler.js
@@ -1,9 +1,12 @@
 const urlLib = require("url");
 const set = require("lodash/set");
+const AmpOptimizer = require("@ampproject/toolbox-optimizer");
 const { Story, AmpConfig } = require("../impl/api-client-impl");
 const { addCacheHeadersToResult } = require("./cdn-caching");
 const { storyToCacheKey } = require("../caching");
 const { InfiniteScrollAmp, setCorsHeaders } = require("../amp-helpers");
+
+const ampOptimizer = AmpOptimizer.create();
 
 function getSeoInstance(seo, config, pageType = "") {
   return typeof seo === "function" ? seo(config, pageType) : seo;
@@ -126,15 +129,16 @@ exports.handleAmpRequest = async function handleAmpRequest(
       infiniteScrollInlineConfig,
     });
     if (ampHtml instanceof Error) return next(ampHtml);
-    res.set("Content-Type", "text/html");
+    const optimizedAmpHtml = await ampOptimizer.transformHtml(ampHtml);
 
+    res.set("Content-Type", "text/html");
     addCacheHeadersToResult(
       res,
       storyToCacheKey(config["publisher-id"], story),
       cdnProvider
     );
 
-    return res.send(ampHtml);
+    return res.send(optimizedAmpHtml);
   } catch (e) {
     return next(e);
   }
