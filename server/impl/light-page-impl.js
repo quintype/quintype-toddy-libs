4d7b712c939b4ad3660a1b7b94f9ea239f2e8c39
Garima Kamboj <garima.kamboj@icloud.com>
2020-09-02 17:12:21 +0530

    Add logs

--- server/impl/light-page-impl.js
+++ server/impl/light-page-impl.js
@@ -1,13 +1,19 @@
 const _ = require("lodash");
 
-function addLightPageHeaders(result, lightPages, { config, res, client, req }) {
+function addLightPageHeaders(result, lightPages, {
+  config,
+  res,
+  client,
+  req
+}) {
   const isAmpSupported = _.get(
     result,
     ["data", "story", "is-amp-supported"],
     false
   );
-
+  console.log("lightPages --------->", lightPages, lightPages(config), "isAmpSupported --------->", isAmpSupported);
   if (typeof lightPages === "function" && !lightPages(config)) {
+    console.log("should't come here ----------------->");
     return;
   }
 
@@ -20,4 +26,6 @@ function addLightPageHeaders(result, lightPages, { config, res, client, req }) {
     );
 }
 
-module.exports = { addLightPageHeaders };
+module.exports = {
+  addLightPageHeaders
+};
