b299ca5a6325dffe3e29042d07a24f9d4f5ccb8d
Deo Kumar <40450564+Deokumarjnu@users.noreply.github.com>
2020-09-10 11:55:50 +0530

    Feat: Support Logging to stdout (#157)

--- server/logger.js
+++ server/logger.js
@@ -48,17 +48,21 @@ function createDevLogger() {
 }
 
 function createProdLogger() {
-  return winston.createLogger({
-    format: combine(timestamp(), trimNewline(), winston.format.json()),
-    transports: [
-      new winston.transports.Console({
-        colorize: false,
-        level: "error",
-      }),
+  const transports = [
+    new winston.transports.Console({
+      colorize: false,
+      level: "error",
+    }),
+  ];
+  !process.env.LOG_TO_STDOUT &&
+    transports.push(
       new winston.transports.File({
         filename: "log/production.log",
-      }),
-    ],
+      })
+    );
+  return winston.createLogger({
+    format: combine(timestamp(), trimNewline(), winston.format.json()),
+    transports,
     exceptionHandlers: [new winston.transports.Console()],
     exitOnError: false,
   });
@@ -84,12 +88,20 @@ function truncateStack(message) {
 
 const logger = createLogger();
 const errorFn = logger.error.bind(logger);
-
+const infoFn = logger.error.bind(logger);
 logger.error = function (e) {
   if (e && e.stack) {
     errorFn({ message: e.message, stack: truncateStack(e.stack) });
   } else {
     errorFn(e);
+  }
+};
+
+logger.info = function (e) {
+  if (e && e.stack) {
+    infoFn({ message: e.message, stack: truncateStack(e.stack) });
+  } else {
+    infoFn(e);
   }
 };
 
---
+++ test/unit/logger-test.js
@@ -0,0 +1,17 @@
+const logger = require("../../server/logger");
+var assert = require("assert");
+
+describe("Logger", function () {
+  it("It should log error while sending logger.error", function (done) {
+    const msg = "Error";
+    const errMsg = logger.error(msg);
+    errMsg && assert.equal(msg, errMsg.message);
+    return done();
+  });
+  it("It should not log error while sending logger.info", function (done) {
+    const msg = "Some info logs";
+    const infoMsg = logger.info(msg);
+    infoMsg && assert.equal(msg, infoMsg.message);
+    return done();
+  });
+});
