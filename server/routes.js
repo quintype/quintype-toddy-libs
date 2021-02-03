038d67c5874fbb20329d3cae59c8160aefa6311d
jeevan-kishore <kishor080@gmail.com>
2021-02-03 10:19:36 +0530

    fix(amp): Add documentation

--- server/routes.js
+++ server/routes.js
@@ -265,6 +265,7 @@ function getWithConfig(app, route, handler, opts = {}) {
  * @param {function} opts.maxConfigVersion An async function which resolves to a integer version of the config. This defaults to config.theme-attributes.cache-burst
  * @param {Array<object>|function} opts.redirectUrls An array or async function which used to render the redirect url provided in the array of object - >ex- REDIRECT_URLS = [{sourceUrl: "/tag/:tagSlug",destinationUrl: "/topic/:tagSlug",statusCode: 301,}]
  * @param {boolean|function} redirectToLowercaseSlugs If set or evaluates to true, then for every story-page request having capital latin letters in the slug, it responds with a 301 redirect to the lowercase slug URL. (default: true)
+ * @param {boolean} shouldEncodeAmpUri If set to true, then for every story-page request the slug will be encoded, in case of a vernacular slug this should be set to false. (default: true)
  */
 exports.isomorphicRoutes = function isomorphicRoutes(
   app,
