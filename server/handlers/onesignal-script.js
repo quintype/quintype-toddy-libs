038bfc2b37630641c233dca8cdf25169367429d8
nandap4790 <nandap4790@gmail.com>
2021-04-08 15:53:18 +0530

    fix(onesignal): revert the refactor

--- server/handlers/onesignal-script.js
+++ server/handlers/onesignal-script.js
@@ -20,13 +20,23 @@ exports.getOneSignalScript = function getOneSignalScript({
       config["public-integrations"]["one-signal"]["app-id"]) ||
     null;
 
-  const onesignalConfig = publisherConfig.publisher && publisherConfig.publisher.onesignal
-
   // will take safariWebId from public integration once bold have option to put safari id.
-  const safariWebId = (onesignalConfig && onesignalConfig.safari_web_id) || null;
-  const isEnable = (onesignalConfig && onesignalConfig.is_enable) || false;
+  const safariWebId =
+    (publisherConfig.publisher &&
+      publisherConfig.publisher.onesignal &&
+      publisherConfig.publisher.onesignal.safari_web_id) ||
+    null;
+  const isEnable =
+    (publisherConfig.publisher &&
+      publisherConfig.publisher.onesignal &&
+      publisherConfig.publisher.onesignal.is_enable) ||
+    false;
   const publisherName = config["publisher-name"] || "malibu";
-  const timeOut = (onesignalConfig && onesignalConfig.time_out) || 4000;
+  const timeOut =
+    (publisherConfig.publisher &&
+      publisherConfig.publisher.onesignal &&
+      publisherConfig.publisher.onesignal.time_out) ||
+    4000;
   const renderedContent = isEnable
     ? partialTemplate({
         appId,
